14b52e27a40020299f91c8155cd109cba28801b2
Oriana González <99359925+Oripau03@users.noreply.github.com>
2025-09-28 13:59:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/ProyectoReact">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
